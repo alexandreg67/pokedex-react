775284d103020bd9e356cf1ee4264e2261f467dd
Alex <alexandre.ag.67@gmail.com>
2024-10-27 15:32:47 +0100

    mise en place du hook useHistory dans pokemoncard pour permettre la navigation vers la page d'accueil

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React, { FunctionComponent } from "react";
 import PokemonList from "./pages/PokemonList";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import PokemonDetail from "./pages/PokemonDetail";
 
 const App: FunctionComponent = () => {
   return (
-    <div>
-      <PokemonList />
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokédex
+            </Link>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/:id" component={PokemonDetail} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
