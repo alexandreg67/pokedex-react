3a840b1ce3d219f6c28a09983ef8ae88fb7ef8c9
Alex <alexandre.ag.67@gmail.com>
2024-11-01 11:15:13 +0100

    suppression du fichier mock et ajout des requêtes fetch pour simuler une requête api

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent, useState, useEffect } from "react";
 import { RouteComponentProps, Link } from "react-router-dom";
 import Pokemon from "../models/pokemon";
-import POKEMONS from "../models/mock-pokemon";
 import formatDate from "../helpers/format-date";
 import formatType from "../helpers/format-type";
 
@@ -13,11 +12,13 @@ const PokemonsDetail: FunctionComponent<RouteComponentProps<Params>> = ({
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
   useEffect(() => {
-    POKEMONS.forEach((pokemon) => {
-      if (match.params.id === pokemon.id.toString()) {
-        setPokemon(pokemon);
-      }
-    });
+    fetch(`http://localhost:3001/pokemons/${match.params.id}`)
+      .then((response) => response.json())
+      .then((pokemon) => {
+        if (pokemon.id) {
+          setPokemon(pokemon);
+        }
+      });
   }, [match.params.id]);
 
   return (
